97b774b7cf48bdfa2f742f5a25bb312be57c99cc
Kathrin92 <kathrinulmer@gmx.de>
2020-03-11 17:35:00 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Add from "./components/pages/Add";
+import Vote from "./components/pages/Vote";
+import Diy from "./components/pages/Diy";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-    </div>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Add />
+          </Route>
+          <Route path="/vote">
+            <Vote />
+          </Route>
+          <Route path="/diy">
+            <Diy />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/components/pages/Diy.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Diy() {
+  return (
+    <div>
+      Diy<Link to="/">Add</Link>
+    </div>
+  );
+}
+export default Diy;
